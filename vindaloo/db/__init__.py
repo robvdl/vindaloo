4312b2d28700e277e247f854ff83b8a5c09b0216
Rob van der Linde <robvdl@gmail.com>
2016-06-05 23:21:29 +1200

    Remove naming convention code as it was causing problems
    This code originally came from the alchemy scaffold in Pyramid 1.7
    
    It was causing issues with some databases like SQLite, when
    boolean fields were used in models, then check constraints were
    automatically added that conflicted with the naming convention.

--- vindaloo/db/__init__.py
+++ vindaloo/db/__init__.py
@@ -1,28 +1,13 @@
-from sqlalchemy.schema import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 from .base import BaseModel
 from .session import get_engine, get_session_factory, get_tm_session, get_dbsession
 
 
-# Recommended naming convention used by Alembic, as various different database
-# providers will autogenerate vastly different names making migrations more
-# difficult. See: http://alembic.readthedocs.org/en/latest/naming.html
-NAMING_CONVENTION = {
-    'ix': 'ix_%(column_0_label)s',
-    'uq': 'uq_%(table_name)s_%(column_0_name)s',
-    'ck': 'ck_%(table_name)s_%(constraint_name)s',
-    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
-    'pk': 'pk_%(table_name)s'
-}
+Model = declarative_base(cls=BaseModel)
 
-metadata = MetaData(naming_convention=NAMING_CONVENTION)
-
-Model = declarative_base(metadata=metadata, cls=BaseModel)
 
 __all__ = [
-    'NAMING_CONVENTION',
-    'metadata',
     'Model',
     'get_engine',
     'get_session_factory',
