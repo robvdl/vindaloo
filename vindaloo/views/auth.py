b85910952df25ef8773d9c96e2447c3b50f79d13
Rob van der Linde <robvdl@gmail.com>
2016-06-19 16:15:24 +1200

    Deal with flash errors from the login form.
    In order for errors to show, we still need to add the error class to the form
    if there are validation errors, in order to do this we use {% set %} in the
    template to create a temporary variable.
    
    Also, on failed login, just create a new form, so the previous username
    doesn't get pre-filled.

--- vindaloo/views/auth.py
+++ vindaloo/views/auth.py
@@ -17,6 +17,8 @@ def login(request):
         if request.login(username, password):
             request.session.flash('Logged in successfully.', queue='success')
         else:
+            # Create a new form so the username isn't pre-populated.
+            form = LoginForm()
             request.session.flash('Invalid username or password.', queue='error')
 
     return {
